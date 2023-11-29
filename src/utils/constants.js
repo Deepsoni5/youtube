5fa8edc6980d1bc36dd675e124e03244548d018b
Deep soni <fichadiyadeep@gmail.com>
2023-11-29 20:12:00 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const GOOGLE_API_KEY = "AIzaSyBDyW11_rY09L72hDH03pvr4eX--Xr644w";
 export const OFFSET_LIVE_CHAT = 13;
-export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "https://youtube-eight-ruby.vercel.app/";
 export const YOUTUBE_VIDEO_API =
   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
